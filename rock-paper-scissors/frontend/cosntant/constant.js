3f924bd5306dca356da2b09379b2a41c3428f4f5
Miracle1000 <158578991+Miracle1000@users.noreply.github.com>
2024-05-10 10:21:15 -0800

    Update constant.js

--- rock-paper-scissors/frontend/cosntant/constant.js
+++ rock-paper-scissors/frontend/cosntant/constant.js
@@ -1,5 +1,6 @@
 export const SERVER_ADDRESS = 'http://192.168.89.3:4000';
 export const ADMIN_WALLET_ADDRESS = '0x0c0dc0c4F1A6b338396db890cFE0229b9138DB27';
+export const CONTRACT_ADDRESS = "0x5c151ce6ef6c3ad23222de63f70077eed6dc806b";
 
 export const REMAIN_BASIC_TIMER = 100;
 export const REMAIN_MIDDLE_TIMER = 10;
@@ -7,7 +8,6 @@ export const REMAIN_HIGH_TIMER = 5;
 
 export const TOKEN_MIDDLE_COUNT = 16;
 export const TOKEN_HIGH_COUNT = 100;
-
 // module.exports = {
 //     SERVER_ADDRESS,
 //     ADMIN_WALLET_ADDRESS,
